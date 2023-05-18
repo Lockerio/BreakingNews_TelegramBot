89a1db0c78615df65b963d171bf2348f30bbb904
Lockerio <goryunov.arkadiy@gmail.com>
2023-05-18 16:21:45 +0700

    Feature(DB): Create database

--- DB/database.py
+++ DB/database.py
@@ -1,13 +1,7 @@
 import sqlalchemy as db
 
 
-engine = db.create_engine('sqlite:///news.db')
-
-metadata = db.MetaData()
-
-
-
-
+engine = db.create_engine('sqlite:///News.db')
 
 
 if __name__ == '__main__':
--- DB/models.py
+++ DB/models.py
@@ -1,12 +1,50 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, Table, Text, ForeignKey
 
-from database import db
+from database import engine
+from sqlalchemy.orm import declarative_base, relationship
 
-users = db.Table("Пользователи", db.MetaData(),
-                 Column("user_id", Integer, primary_key=True)
+Base = declarative_base()
+
+Favorites = Table('Favorites', Base.metadata,
+                  Column('agency_id', Integer(), ForeignKey('NewsAgencies.id')),
+                  Column('user_id', Integer(), ForeignKey('Users.id'))
+                  )
 
 
-                 )
+class NewsAgency(Base):
+    __tablename__ = 'NewsAgencies'
+    id = Column(Integer(), primary_key=True)
+    name = Column(Text())
+    description = Column(Text())
+    news = relationship("News", back_populates="news_agency")
+
+    def __repr__(self):
+        return f'{self.name}'
+
+
+class User(Base):
+    __tablename__ = 'Users'
+    id = Column(Integer(), primary_key=True)
+    telegram_id = Column(Integer())
+    news_amount_to_show = Column(Integer())
+    news_agencies = relationship("NewsAgency", secondary=Favorites, backref="users")
+
+    def __repr__(self):
+        return f'{self.telegram_id}'
+
+
+class News(Base):
+    __tablename__ = 'News'
+    id = Column(Integer(), primary_key=True)
+    title = Column(Text())
+    text = Column(Text())
+    url = Column(Text())
+    news_agency_id = Column(Integer(), ForeignKey('NewsAgencies.id'))
+    news_agency = relationship("NewsAgency", back_populates="news")
+
+    def __repr__(self):
+        return f'{self.title}'
+
 
 if __name__ == '__main__':
-    pass
+    Base.metadata.create_all(engine, checkfirst=True)
