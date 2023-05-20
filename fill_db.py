fb7b3a4a5ed4b8b68980b3c6e54a3f225917d88f
Lockerio <goryunov.arkadiy@gmail.com>
2023-05-20 14:16:40 +0700

    Feature(fill_db): Write script to fill db

--- fill_db.py
+++ fill_db.py
@@ -2,10 +2,12 @@ import os
 
 from container import agencyService
 from parsing.BaikalDaily.baikal_daily import BaikalDailyParser
-from parsing.meta import BAIKAL_DAILY_URL
+from parsing.CityN.city_n import CityNParser
+from parsing.IrkRu.irk_ru import IrkRuParser
+from parsing.meta import BAIKAL_DAILY_URL, IRK_RU_URL, CITY_N_URL
 
 
-def fill_db():
+def fill_db_agencies():
     agencies = [
         {
             "name": "Байкал Daily",
@@ -28,13 +30,34 @@ def fill_db():
         agencyService.create(agency)
 
 
-if __name__ == '__main__':
-    fill_db()
-
+def fill_db_news():
     current_dir = os.getcwd()
-    folder_path = os.path.join(current_dir, 'parsing', 'BaikalDaily')
-    file_path = os.path.join(folder_path, 'index.html')
+
+    # BaikalDaily
+    baikal_daily_folder_path = os.path.join(current_dir, 'parsing', 'BaikalDaily')
+    baikal_daily_file_path = os.path.join(baikal_daily_folder_path, 'index.html')
 
     baikalDailyParser = BaikalDailyParser(BAIKAL_DAILY_URL)
-    news = baikalDailyParser.find_news(file_path)
+    news = baikalDailyParser.find_news(baikal_daily_file_path)
     baikalDailyParser.save_news_to_db(news)
+
+    # IrkRu
+    irk_ru_folder_path = os.path.join(current_dir, 'parsing', 'IrkRu')
+    irk_ru_file_path = os.path.join(irk_ru_folder_path, 'index.html')
+
+    irkRuParser = IrkRuParser(IRK_RU_URL)
+    news = irkRuParser.find_news(irk_ru_file_path)
+    irkRuParser.save_news_to_db(news)
+
+    # CityN
+    city_n_folder_path = os.path.join(current_dir, 'parsing', 'CityN')
+    city_n_file_path = os.path.join(city_n_folder_path, 'index.html')
+
+    cityNParser = CityNParser(CITY_N_URL)
+    news = cityNParser.find_news(city_n_file_path)
+    cityNParser.save_news_to_db(news)
+
+
+if __name__ == '__main__':
+    fill_db_agencies()
+    fill_db_news()
